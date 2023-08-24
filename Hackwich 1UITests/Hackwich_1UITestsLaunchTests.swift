57f099127edf61390d67f3771aea74c1c4492145
Tani Umetsu <taniu@hawaii.edu>
2023-08-24 13:18:45 -1000

    Initial Commit

---
+++ Hackwich 1UITests/Hackwich_1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hackwich_1UITestsLaunchTests.swift
+//  Hackwich 1UITests
+//
+//  Created by Tani Umetsu on 8/24/23.
+//
+
+import XCTest
+
+final class Hackwich_1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
